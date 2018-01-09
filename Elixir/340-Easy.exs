2ea48b3365763d64f64e0333e8eaa67fca1c1279
mochancrimthann <fauxfoxfx@gmail.com>
2018-01-09 14:47:07 -0500

    Working on 340-Easy, moving computers

--- Elixir/340-Easy.exs
+++ Elixir/340-Easy.exs
@@ -1,3 +1,12 @@
 defmodule Pattern do
-  def first(str)
+  def first(str) do
+    str
+    |> String.graphemes
+    |> get_dups
+    |> Enum.find fn x -> 
+  end
+
+  def get_dups(letters), do: letters -- Enum.uniq(letters)
 end
+
+IO.inspect Pattern.first("ABBA")
