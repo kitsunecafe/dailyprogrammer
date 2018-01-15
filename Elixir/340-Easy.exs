9ec1e0ff38941d3f4cb788068955ec15346d9584
mochancrimthann <fauxfoxfx@gmail.com>
2018-01-15 12:51:52 -0500

    Finished 340-Easy.exs and 344-Intermediate.js

--- Elixir/340-Easy.exs
+++ Elixir/340-Easy.exs
@@ -1,12 +1,17 @@
 defmodule Pattern do
-  def first(str) do
+  def nb_first(str) do
     str
     |> String.graphemes
     |> get_dups
-    |> Enum.find fn x -> 
+    |> Enum.at(0)
   end
 
   def get_dups(letters), do: letters -- Enum.uniq(letters)
+
+  def first(str), do: first(String.graphemes(str), %{}, str)
+  defp first([], _, _), do: :not_found
+  defp first([letter | _], %{^letter => _}, _), do: letter
+  defp first([letter | rest], acc, str), do: first(rest, Map.put(letters, letter, Enum.find_index(str, fn x -> x == letter end)))
 end
 
-IO.inspect Pattern.first("ABBA")
+IO.inspect Pattern.first("ABCDEBC")
