1d2bc1f2d69bf5dfc7588a579ccf20d7e2ab2d68
mochancrimthann <fauxfoxfx@gmail.com>
2018-04-23 12:43:14 -0400

    Small change to 352-Easy.hs

--- Haskell/352-Easy.hs
+++ Haskell/352-Easy.hs
@@ -3,4 +3,4 @@ b62 :: Integer -> String
 b62 n 
   | n <= 62   = [char]
   | otherwise = char:b62 (div n 62) 
-  where char  = (['0'..'9']++['a'..'z']++['A'..'Z']) !! fromInteger(n `mod` 62)
+  where char  = (['0'..'9']++['a'..'z']++['A'..'Z']) !! fromInteger(mod n 62)
